a32aead606e0fa41e28d1f2a5d8e810d227398c3
Stepan <kurakste@gmail.com>
2019-03-01 20:24:53 +0300

    add sort method))))

--- helpers/arrayhelpers.js
+++ helpers/arrayhelpers.js
@@ -1,11 +1,25 @@
+// It gets min value from array
 exports.min = (arr) => {
     return arr.reduce( 
         (acc, el) => (el < acc) ? el : acc
         , Infinity ); 
 }
 
+// It gets max value from array
 exports.max = (arr) => {
     return arr.reduce( (acc, el) => {
         return (el > acc) ? el : acc;
     }, -Infinity)
+}
+
+// It get array and return sorted one.
+exports.sort = (arr) => {
+    let out = arr.sort( (a,b) => {
+        if (a>b) return -1;
+        if (a<b) return 1;
+        if (a === b) return 0;
+
+    });
+    console.log(out);
+    return out;
 }
--- helpers/arrayhelpers.test.js
+++ helpers/arrayhelpers.test.js
@@ -4,4 +4,6 @@ const ah = require('./arrayhelpers');
 test('min value test', () => {
       expect(ah.min([1, 2, 3, 4, -8, 2])).toBe(-8);
       expect(ah.max([1, 2, 3, 4, -8, 2])).toBe(4);
+      expect(ah.sort([1, 2, 3, 4, -8, 20])).toEqual([ 20, 4, 3, 2, 1, -8 ]);
+      expect(ah.sort([1, 2, 3, 4, -8, 20])).not.toEqual([ 20, 4, 2, 3, 1, -8 ]);
 });
