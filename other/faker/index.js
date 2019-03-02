6bbac0bb047325e5220ac84e3f52dfbfaa24a4df
Stepan <kurakste@gmail.com>
2019-03-02 18:46:25 +0300

    playing aroun faker

---
+++ other/faker/index.js
@@ -0,0 +1,24 @@
+const faker = require('faker');
+const fs = require('fs');
+const producGen = () => {
+    return {
+        name: faker.commerce.productName(),
+        category: faker.commerce.product(),
+        color: faker.commerce.color(),
+        price: faker.commerce.price(),
+        producer: faker.random.alphaNumeric(10),
+        }
+};
+
+data = []
+for (let i = 0; i< 1000000; i++) {
+    data.push(producGen());
+}
+
+fs.writeFile(
+    './data/goods.json'
+    , JSON.stringify(data)
+    , err => {
+        console.log(err);
+});
+console.log('work is done');
