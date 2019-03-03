fcc51a64c5924786bb017d85c5c3b299d7ab1f6d
Stepan <kurakste@gmail.com>
2019-03-03 11:02:43 +0300

    yeld elemtn with dalay

---
+++ other/simply/yeldwithdalay.js
@@ -0,0 +1,18 @@
+const data = [12, 3, 55, 6];
+
+yellder = (arr, counter = 0) => {
+    if (counter === arr.length) return 0; 
+
+    let el = arr[counter];
+    let i = counter;
+
+
+    setTimeout(
+        () => { 
+            console.log(`The element with index ${i} is ${el}.`)
+            yellder(arr, ++i);
+        }
+        ,3000);
+};
+
+yellder(data);
