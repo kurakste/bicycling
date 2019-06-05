c9a8d591f11c4fb91ce8f46f7a6c59ce439b985a
Stepan <kurakste@gmail.com>
2019-06-05 22:05:57 +0300

    test arguments

--- mailru/fibonachi.js
+++ mailru/fibonachi.js
@@ -1,4 +1,5 @@
 function fibonachi(n) {
+  if (typeof n !== 'number') throw new Error('Argument must be a nubmer');
   if (n === 0) return 0;
   if (n === 1) return 1;
   return fibonachi(n-2) + fibonachi(n-1)
--- mailru/fuzzbuzz.js
+++ mailru/fuzzbuzz.js
@@ -1,6 +1,6 @@
 function fuzzbuzz(inp) {
   let res = '';
-  if (typeof inp !== 'number') throw new Error('Argument must be Nubmer');
+  if (typeof inp !== 'number') throw new Error('Argument must be a nubmer');
   if ((inp % 3 === 0) || (((inp.toString()).search('3') !==-1))) res = 'fuzz';
   if ((inp % 5 === 0) || (((inp.toString()).search('5') !==-1))) res += 'buzz';
   return res;
